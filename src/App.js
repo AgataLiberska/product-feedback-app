a6ed221e70bcd05b744bcafc435f250b7fc38b3a
Agata Liberska <liberska@gmail.com>
2021-07-13 13:53:44 +0100

    add react router to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import SuggestionsList from './features/suggestions/SuggestionsList';
 
 const App = () => {
     return (
-        <>
-        <div> App </div>
-        <SuggestionsList />
-        </>
+        <BrowserRouter>
+            <Switch>
+                <Route 
+                    exact 
+                    path='/'
+                    render={() => (
+                        <>
+                            <header>
+                                <h1>FrontEndMentor</h1>
+                                <p>Suggestions Board</p>
+                            </header>
+                            <SuggestionsList />
+                        </>
+                    )}
+                />
+                <Redirect to='/' />
+            </Switch>
+            
+        </BrowserRouter>
     )
 }
 
